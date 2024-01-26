b4aa90997253356200a0fcf078ca38a633dcefab
Ch3my <joseconchan1@gmail.com>
2024-01-26 14:21:02 -0300

    Update to Expo 50

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["module:metro-react-native-babel-preset", "babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
       },
     },
-    plugins: [
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
-    ],
   };
 };
